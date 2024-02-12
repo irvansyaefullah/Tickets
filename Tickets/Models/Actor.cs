004b83a5b8c774d985b92caed4db7c49a6bb828b
irvansyaefullah <irvansyaefullah07@gmail.com>
2024-02-12 23:34:41 +0700

    Finish CRUD Actor

--- Tickets/Models/Actor.cs
+++ Tickets/Models/Actor.cs
@@ -21,6 +21,6 @@ namespace Tickets.Models
         public string Bio { get; set; }
 
         //Relationships
-        public List<Actor_Movie> Actors_Movies { get; set; }
+        public List<Actor_Movie>? Actors_Movies { get; set; }
     }
 }
